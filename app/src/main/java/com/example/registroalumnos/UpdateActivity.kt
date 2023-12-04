4be5f6b37b935863ca3c30380747eaebd9058863
RaulGonzalezLeon <raul.gonzalez.leon.04@gmail.com>
2023-12-04 11:13:51 +0100

    ACTIVIDAD 14

--- app/src/main/java/com/example/registroalumnos/UpdateActivity.kt
+++ app/src/main/java/com/example/registroalumnos/UpdateActivity.kt
@@ -2,13 +2,57 @@ package com.example.registroalumnos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.registroalumnos.database.dataAlumno
+import com.example.registroalumnos.database.miAlumnoDB
 import com.example.registroalumnos.databinding.ActivityMainBinding
 import com.example.registroalumnos.databinding.ActivityUpdateBinding
+import com.example.registroalumnos.mdatabase.miAlumno
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UpdateActivity : ActivityWithMenus() {
+    lateinit var binding : ActivityUpdateBinding
+    lateinit var listaAlumnos: MutableList<dataAlumno>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityUpdateBinding.inflate(layoutInflater)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.botonActualizar.setOnClickListener(){
+
+            var nombreAlumno = binding.actualizarNombre.text.toString()
+            var cursoAlumno = binding.actualizarCurso.text.toString()
+
+
+            // Validaciones
+            if (nombreAlumno.isEmpty())
+            {
+                Toast.makeText(this, "No puede haber campos vacíos", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+
+                updateAlumno(nombreAlumno, cursoAlumno)
+                Toast.makeText(this, "Alumno eliminado", Toast.LENGTH_SHORT).show()
+
+
+
+
+            }
+        }
+    }
+
+    fun updateAlumno(alumno: String, curso:String){
+        CoroutineScope(Dispatchers.IO).launch{
+            val alumno = miAlumno.database.alumnoDAO().obteneralumnopornombre(alumno)
+
+            val listaAlumnos = alumno[0]
+            listaAlumnos.curso = curso
+            miAlumno.database.alumnoDAO().updateLista(listaAlumnos)
+
+
+        }
     }
 }
